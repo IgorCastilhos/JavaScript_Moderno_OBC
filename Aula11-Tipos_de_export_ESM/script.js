f581e9654baaacb745b934cd88c71ec628e78ec7
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-01-15 15:11:24 -0300

    another commit

--- Aula11-Tipos_de_export_ESM/script.js
+++ Aula11-Tipos_de_export_ESM/script.js
@@ -1,3 +1,8 @@
+import { inline } from "./inline.js";
+import defaultInline from "./inline.js";
+import { group } from "./non-inline.js";
+import exportDefault from "./non-inline.js";
+
 inline();
 defaultInline();
 
